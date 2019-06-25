8a7b957cfac32f3f3a7b7f008fe226d45e1a1538
parkertleavitt@gmail.com <parkertleavitt@gmail.com>
2019-06-25 18:24:23 -0500

    create base components

--- components-template-app/src/app/app.module.ts
+++ components-template-app/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { FormsComponent } from './forms/forms.component';
+import { TablesComponent } from './tables/tables.component';
+import { ChartsComponent } from './charts/charts.component';
+import { HomeComponent } from './home/home.component';
+import { MiscUtilitiesComponent } from './misc-utilities/misc-utilities.component';
+import { LayoutsComponent } from './layouts/layouts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsComponent,
+    TablesComponent,
+    ChartsComponent,
+    HomeComponent,
+    MiscUtilitiesComponent,
+    LayoutsComponent
   ],
   imports: [
     BrowserModule,
---
+++ components-template-app/src/app/misc-utilities/misc-utilities.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-misc-utilities',
+  templateUrl: './misc-utilities.component.html',
+  styleUrls: ['./misc-utilities.component.css']
+})
+export class MiscUtilitiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
