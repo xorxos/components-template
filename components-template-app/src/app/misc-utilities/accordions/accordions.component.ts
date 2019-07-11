5399a8ffc96088ba5855079c54549e656f07fd60
parkertleavitt@gmail.com <parkertleavitt@gmail.com>
2019-07-10 19:55:29 -0500

    build accordions component

--- components-template-app/src/app/misc-utilities/accordions/accordions.component.ts
+++ components-template-app/src/app/misc-utilities/accordions/accordions.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatAccordion } from '@angular/material';
 
 @Component({
   selector: 'app-accordions',
   templateUrl: './accordions.component.html',
   styleUrls: ['./accordions.component.scss']
 })
-export class AccordionsComponent implements OnInit {
+export class AccordionsComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  @ViewChild(MatAccordion) accordion: MatAccordion;
+  displayMode: string = 'default';
+  multi = false;
+  hideToggle = false;
+  disabled = false;
+  expandedHeight: string;
+  collapsedHeight: string;
 
 }
